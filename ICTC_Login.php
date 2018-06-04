b7bda495f49ff3529d8218da4d5b15a5ec171a97
Unknown <jonathanerikpadua@gmail.com>
2018-06-05 06:43:22 +0800

    dbconnect 5

--- ICTC_Login.php
+++ ICTC_Login.php
@@ -32,7 +32,7 @@ else {
 $password=$_POST['password'];
 $query1="SELECT * FROM admin WHERE username='$username' AND password='$password'";
 
-$result1= mysql_query($query1);
+$result1= mysql_query($query1) or die(mysql_error($conn));
 $count1= mysql_num_rows($result1);
 
 if ($count1 == 1){
